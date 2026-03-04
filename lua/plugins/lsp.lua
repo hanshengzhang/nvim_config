c183979c504d18b0d360ed925da23514530c835d
zhanghansheng <zhanghansheng@bytedance.com>
2026-03-04 10:20:28 +0800

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -49,7 +49,7 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
-    config = function()
+    init = function()
       vim.g.rustaceanvim = {
         server = {
           on_attach = function(_, bufnr)
